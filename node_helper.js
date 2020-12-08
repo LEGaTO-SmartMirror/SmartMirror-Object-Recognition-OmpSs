c3e574d52e329a80b6c6b612a227dc9bbea9e006
Nils Kucza <nkucza@cit-ec.uni-bielefeld.de>
2020-12-08 15:50:24 +0100

    restart if process unexpectetly dies

--- node_helper.js
+++ node_helper.js
@@ -8,7 +8,7 @@ module.exports = NodeHelper.create({
 
 	cApp_start: function () {
 		const self = this;
-		self.objectDet = spawn('modules/' + this.name + '/object_detection_ompss/build/start.sh',['modules/' + this.name + '/object-detection/build', self.config.image_width, self.config.image_height]);
+		self.objectDet = spawn('modules/' + self.name + '/object_detection_ompss/build/start.sh',['modules/' + self.name + '/object-detection/build', self.config.image_width, self.config.image_height]);
 		self.objectDet.stdout.on('data', (data) => {
 
 			var data_chunks = `${data}`.split('\n');
@@ -30,74 +30,60 @@ module.exports = NodeHelper.create({
 					}
 				}
 				catch(err) {	
-					//console.log(err)
+					if (err.message.includes("Unexpected token") && err.message.includes("in JSON")){
+						//console.log("[" + self.name + "] json parse error");
+					} else if (err.message.includes("Unexpected end of JSON input")) {
+						console.log("[" + self.name + "] Unexpected end of JSON input")
+					} else {
+						console.log(err.message)
+					}
 				}
   				//console.log(chunk);
 				}
 			});
 		});	
 
-
-
-		/*exec(`renice -n 20 -p ${self.gestureDet.pid}`,(error,stdout,stderr) => {
+		exec(`renice -n 20 -p ${self.objectDet.pid}`,(error,stdout,stderr) => {
 				if (error) {
 					console.error(`exec error: ${error}`);
   				}
-			}); */
+			});
+
+		self.objectDet.on("exit", (code, signal) => {
+				if (code !== 0){
+					setTimeout(() => {self.cApp_start();}, 10)
+				}				
+				console.log("object det:");
+				console.log("code: " + code);
+				console.log("signal: " + signal);
+
+		});
 
 		},
 
-		/*	var data_chunks = `${data}`.split('\n');
-			data_chunks.forEach( chunk => {
-
-				if (chunk.length > 0) {
-
-			//console.log(`${data}`)
-			try{
-				var parsed_message = JSON.parse(chunk)
-
-				if (parsed_message.hasOwnProperty('DETECTED_OBJECTS')){
-					console.log("[" + self.name + "] detected object: " + parsed_message.detected.name + " center in "  + parsed_message.detected.center);
-					self.sendSocketNotification('DETECTED_OBJECTS', parsed_message);
-				}else if (parsed_message.hasOwnProperty('OBJECT_DET_FPS')){
-					console.log("[" + self.name + "] object detection fps: " + JSON.stringify(parsed_message));
-					self.sendSocketNotification('OBJECT_DET_FPS', parsed_message.OBJECT_DET_FPS);
-				}else if (parsed_message.hasOwnProperty('STATUS')){
-					console.log("[" + self.name + "] status received: " + JSON.stringify(parsed_message));
-				}
-			}
-			catch(err) {	
-					//console.log(err)
-				}
-  				//console.log(chunk);
-				}
-			});
-		});	
-
-
 	
-		/*exec(`renice -n 20 -p ${self.objectDet.pid}`,(error,stdout,stderr) => {
-				if (error) {
-					console.error(`exec error: ${error}`);
-  				}
-			}); */
 
-  	//}, 
 
   	// Subclass socketNotificationReceived received.
   	socketNotificationReceived: function(notification, payload) {
 		const self = this;	
 		if(notification === 'ObjectDetection_SetFPS') {
 			if(cAppStarted) {
-				self.objectDet.stdin.write(payload.toString() + "\n");
-				console.log("[" + self.name + "] changing to: " + payload.toString() + " FPS");
-
-         		}
-       	 	}else if(notification === 'OBJECT_DETECITON_CONFIG') {
+				try{
+					self.objectDet.stdin.write(payload.toString() + "\n");
+					console.log("[" + self.name + "] changing to: " + payload.toString() + " FPS");
+				}
+				catch(err){
+						console.log(err)
+						console.log(err.name)
+						console.log(err.message)
+				}
+			}
+		}else if(notification === 'OBJECT_DETECITON_CONFIG') {
       			self.config = payload
       			if(!cAppStarted) {
         			cAppStarted = true;
-        			this.cApp_start();
+        			self.cApp_start();
       			};
     		};
   	},
